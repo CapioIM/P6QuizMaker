3f9e81ebf2f668b04373c8b38102369b22e03b5c
CapioIM <igorsmalinovskis9@gmail.com>
2023-10-13 13:06:32 +0100

    Add project files.

---
+++ P6QuizMaker/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace P6QuizMaker
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
