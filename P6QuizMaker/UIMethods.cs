52b7163aee558ad5ae0109752ae8659ba5e2994f
CapioIM <igorsmalinovskis9@gmail.com>
2023-10-22 16:33:00 +0100

    welcome text added

---
+++ P6QuizMaker/UIMethods.cs
@@ -0,0 +1,20 @@
+﻿namespace P6QuizMaker
+{
+    internal class UIMethods
+    {
+        public static void WelcomeText()
+        {
+            Console.WriteLine("                 Welcome to Quiz Maker Program !\n" +
+                "You will be asked to type questions and answers to these questions.\n" +
+                "After you will enter answers program will ask you to pick if answer is correct.\n" +
+                "And lastly program will bring random question with answers.\n");
+        }
+
+        public static string GetUserInput()
+        {
+           string textMessage = Console.ReadLine();
+
+            throw new NotImplementedException();
+        }
+    }
+}
