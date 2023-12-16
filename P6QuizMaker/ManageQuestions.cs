0fb6da2c301dbfe8300bca9235ece2924741c354
CapioIM <131615709+CapioIM@users.noreply.github.com>
2023-12-16 18:02:59 +0000

    bad naming , renamed

--- P6QuizMaker/ManageQuestions.cs
+++ P6QuizMaker/ManageQuestions.cs
@@ -5,28 +5,28 @@
         /// <summary>
         /// add answer to specified object
         /// </summary>
-        /// <param name="quizmaker"> object name, if in list where to find specific object </param>
-        public static void AddAnswersToQuestion(QuestionsAndAnswers quizmaker)
+        /// <param name="question"> object name, if in list where to find specific object </param>
+        public static void AddAnswersToQuestion(QuestionsAndAnswers question)
         {
             bool addMoreAnswers = true;
             while (addMoreAnswers)
             {
                 string answerText = UIMethods.GetAndDisplayTypeAnswerText();
-                quizmaker.AnswersList.Add(answerText);
-                AddCorrectAnswersToList(answerText, quizmaker);
+                question.AnswersList.Add(answerText);
+                AddCorrectAnswersToList(answerText, question);
                 addMoreAnswers = UIMethods.GetAdditionalAnswer();
             }
         }
 
-        private static void AddCorrectAnswer(QuestionsAndAnswers quizmaker)
+        private static void AddCorrectAnswer(QuestionsAndAnswers question)
         {
             int answerNumber;
             bool CorrectAnswerDuplicateCheck = true;
             bool addMoreAnswers = true;
             while (addMoreAnswers)
             {
-                answerNumber = UIMethods.DiplayGetNumberText(quizmaker);
-                foreach (int correctAnswerNumber in quizmaker.CorrectAnswersList)
+                answerNumber = UIMethods.DiplayGetNumberText(question);
+                foreach (int correctAnswerNumber in question.CorrectAnswersList)
                 {
                     if (correctAnswerNumber == (answerNumber - 1))
                     {
@@ -35,7 +35,7 @@
                 }
                 if (CorrectAnswerDuplicateCheck)
                 {
-                    quizmaker.CorrectAnswersList.Add(answerNumber - 1);
+                    question.CorrectAnswersList.Add(answerNumber - 1);
                 }
                 else
                 {
@@ -49,25 +49,25 @@
         /// if true adds index of matching string in AnswersList
         /// </summary>
         /// <param name="answerText">input string of Answer</param>
-        /// <param name="quizmaker">Object variable name</param>
-        private static void AddCorrectAnswersToList(string answerText, QuestionsAndAnswers quizmaker)
+        /// <param name="question">Object variable name</param>
+        private static void AddCorrectAnswersToList(string answerText, QuestionsAndAnswers question)
         {
             if (UIMethods.IsCorrectAnswer())
             {
-                quizmaker.CorrectAnswersList.Add(quizmaker.AnswersList.IndexOf(answerText));
+                question.CorrectAnswersList.Add(question.AnswersList.IndexOf(answerText));
             }
         }
 
-        private static QuestionsAndAnswers AddNewQuestion(List<QuestionsAndAnswers> QuizmakerList)
+        private static QuestionsAndAnswers AddNewQuestion(List<QuestionsAndAnswers> questionList)
         {
-            QuestionsAndAnswers quizmakerQuestion = new QuestionsAndAnswers();
-            QuizmakerList.Add(quizmakerQuestion);
-            return quizmakerQuestion;
+            QuestionsAndAnswers newQuestion = new QuestionsAndAnswers();
+            questionList.Add(newQuestion);
+            return newQuestion;
         }
 
         public static void Manage()
         {
-            List<QuestionsAndAnswers> QuizmakerList = FileOperations.DeserializeTest();
+            List<QuestionsAndAnswers> questionList = FileOperations.DeserializeTest();
 
             bool amending = true;
             while (amending)
@@ -86,7 +86,7 @@
 
                 if (modificationTarget == ModificationTarget.AnswerList || modificationTarget == ModificationTarget.CorrectAnswerList)                   // if not questions than do this
                 {
-                    questionToAmend = UIMethods.ShowAnswersListInfo(QuizmakerList, modificationTarget);
+                    questionToAmend = UIMethods.ShowAnswersListInfo(questionList, modificationTarget);
                 }
 
                 ModificationOptions modificationOptions = ModificationOptions.Add;
@@ -101,11 +101,11 @@
                     continue;
                 }
 
-                QuestionsAndAnswers questionReference = QuizmakerList[questionToAmend];
+                QuestionsAndAnswers questionReference = questionList[questionToAmend];
                 switch (modificationTarget)
                 {
                     case ModificationTarget.Questions:                                                                  //questions
-                        ModifyQuestionsOptions(modificationOptions, QuizmakerList, questionReference, questionToAmend);
+                        ModifyQuestionsOptions(modificationOptions, questionList, questionReference, questionToAmend);
                         break;
 
                     case ModificationTarget.AnswerList:                                                             // Answer List
@@ -117,7 +117,7 @@
                         break;
                     case ModificationTarget.SaveChanges:
                         {
-                            FileOperations.CreateXMLSerializeFile(QuizmakerList);                           // create xmlSerialization file
+                            FileOperations.CreateXMLSerializeFile(questionList);                           // create xmlSerialization file
                             continue;
                         }
                 }
@@ -142,28 +142,28 @@
         /// This method modifies question object , add new question, remove existing question, amend question text
         /// </summary>
         /// <param name="modificationOptions"> (user)choice of option </param>
-        /// <param name="QuizmakerList"> List with questions </param>
+        /// <param name="questionList"> List with questions </param>
         /// <param name="questionReference"> question reference to amend question Text </param>
         /// <param name="questionToAmend"> variable which question in list to make changes to </param>
-        private static void ModifyQuestionsOptions(ModificationOptions modificationOptions, List<QuestionsAndAnswers> QuizmakerList, QuestionsAndAnswers questionReference, int questionToAmend)
+        private static void ModifyQuestionsOptions(ModificationOptions modificationOptions, List<QuestionsAndAnswers> questionList, QuestionsAndAnswers questionReference, int questionToAmend)
         {
 
             if (modificationOptions != ModificationOptions.Add)
             {
-                UIMethods.ShowListOfQuestion(QuizmakerList);
+                UIMethods.ShowListOfQuestion(questionList);
                 UIMethods.DisplayTextQuestionToRemoveOrAmend(modificationOptions);
-                questionToAmend = UIMethods.GetUserInputNum(QuizmakerList.Count) - 1;
+                questionToAmend = UIMethods.GetUserInputNum(questionList.Count) - 1;
             }
 
             switch (modificationOptions)
             {
                 case ModificationOptions.Add:                                                               //add
-                    QuestionsAndAnswers question = AddNewQuestion(QuizmakerList);
+                    QuestionsAndAnswers question = AddNewQuestion(questionList);
                     question.QuestionText = UIMethods.GetQuestionText();
                     AddAnswersToQuestion(question);
                     break;
                 case ModificationOptions.Remove:                                                            //remove
-                    QuizmakerList.RemoveAt(questionToAmend);
+                    questionList.RemoveAt(questionToAmend);
                     break;
                 case ModificationOptions.Amend:                                                             // amend
                     UIMethods.ModifyQuestionText(questionReference);
